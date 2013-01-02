0376dd5d53acd8737f6fdb21fc4209b18f631884
Manish C <manish.c@multunus.com>
2013-01-02 16:32:32 +0530

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,76 +1,32 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  describe "Home Page" do
-    it "should contain text 'home'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit "/static_pages/home"
-      page.should have_content("home")
-    end
-  end
 
-  describe "About Page" do
-    it "should contain text 'about'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit "/static_pages/about"
-      page.should have_content("about")
-    end
+  subject { page }
+
+  describe "Home Page" do
+    before {visit root_path}
+
+    it { should have_selector('h1',text:'Sample App')}
+    it { should have_selector('title',text: full_title('')) }
   end
 
   describe "Help Page" do
-    it "should contain text 'help'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit "/static_pages/help"
-      page.should have_content("help")
-    end
+    before {visit help_path}
+
+    it { should have_selector('title',text: full_title('Help')) }
   end
 
-  describe "Contact Us Page" do
-    it "should contain text 'Call'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit "/static_pages/contact"
-      page.should have_content("Call")
-    end
+  describe "About Page" do
+    before {visit about_path}
+
+    it { should have_selector('title',text: full_title('About')) }
   end
 
-  describe "Contact Us Page" do
-    it "should have the right heading 'Contact Us'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit "/static_pages/contact"
-      page.should have_selector("h1",text:"Contact Us")
-    end
-  end
+  describe "Contact Page" do
+    before {visit contact_path}
 
-  describe "Home Page Title" do
-    it "should NOT have the the keyword Home" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit "/static_pages/home"
-      page.should_not have_selector("title",text:"|")
-    end
-  end
-
-  describe "Help Page Title" do
-    it "should have the the keyword Help" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit "/static_pages/help"
-      page.should have_selector("title",text:"Help")
-    end
-  end
-
-  describe "About Page Title" do
-    it "should have the the keyword About" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit "/static_pages/about"
-      page.should have_selector("title",text:"About")
-    end
-  end
-
-  describe "Contact Us Page Title" do
-    it "should have the the keyword 'Contact Us'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit "/static_pages/contact"
-      page.should have_selector("title",text:"Contact Us")
-    end
+    it { should have_selector('title',text: full_title('Contact')) }
   end
 
 end
---
+++ spec/support/utilities.rb
@@ -0,0 +1,5 @@
+def full_title(page_title)
+	base_title = "Welcome to Sample App"
+	return base_title if page_title.empty?
+	"#{base_title} | #{page_title}"
+end
